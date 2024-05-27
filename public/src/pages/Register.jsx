7f0d5076ba3d0374c6662a4cd92e2ea1d9048778
Saksham Verma <sakshamverma626@gmail.com>
2024-05-28 01:02:05 +0530

    moving to mac

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Register() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("from")
+    }
   return (
-    <div>
-      register
-    </div>
+    <>
+      <FromContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+            <div className="brand">
+                <img src="" alt="" />
+                <h1>BroCode</h1>
+            </div>
+            <input type="text" name="username" id="username" placeholder='Username' onChange={e=>handleChange(e)}/>
+            <input type="email" name="email" id="email" placeholder='Email' onChange={e=>handleChange(e)}/>
+            <input type="password" name="password" id="pass" placeholder='Password' onChange={e=>handleChange(e)}/>
+            <input type="password" name="confirmPassword" id="pass" placeholder='Confirm password' onChange={e=>handleChange(e)}/>
+        </form>
+    </FromContainer>
+    </>
   )
 }
-
+const FromContainer = styled.div``;
 export default Register
