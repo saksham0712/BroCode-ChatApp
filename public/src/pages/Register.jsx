bc0ceb280b853d1fdf1cdc9d2bcd72bc1a55b289
Saksham Verma <sakshamverma626@gmail.com>
2024-05-28 20:22:43 +0530

    Login Function

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,30 +1,74 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import Logo from '../assets/logo.png'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios';
+import { registerRoute } from '../utils/APIRoutes';
 function Register() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("from")
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  }
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      console.log('In vaildation', registerRoute)
+      const { password, confirmPassword, username, email } = values;
+      const {data} = await axios.post(registerRoute, {username, email, password,});
     }
-    const handleChange = (event) => {}
-    
+  }
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error("password and confirm password should be same.", toastOptions);
+      return false;
+    } else if (username.length < 3) {
+      toast.error("username should be greater than 3 characters", toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error("Password should be equal or greater than 8 characters", toastOptions);
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required", toastOptions);
+      return false
+    }
+    return true;
+  }
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value })
+  }
+
   return (
     <>
       <FromContainer>
-        <form onSubmit={(event)=>handleSubmit(event)}>
-            <div className="brand">
-                <img src={Logo} alt="" />
-                <h1>BroCode</h1>
-            </div>
-            <input type="text" name="username" id="username" placeholder='Username' onChange={e=>handleChange(e)}/>
-            <input type="email" name="email" id="email" placeholder='Email' onChange={e=>handleChange(e)}/>
-            <input type="password" name="password" id="pass" placeholder='Password' onChange={e=>handleChange(e)}/>
-            <input type="password" name="confirmPassword" id="pass" placeholder='Confirm password' onChange={e=>handleChange(e)}/>
-            <button type="submit">Create User</button>
-            <span>Already have an account ?<Link to='/login'>Login</Link></span>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="" />
+            <h1>BroCode</h1>
+          </div>
+          <input type="text" name="username" id="username" placeholder='Username' onChange={e => handleChange(e)} />
+          <input type="email" name="email" id="email" placeholder='Email' onChange={e => handleChange(e)} />
+          <input type="password" name="password" id="pass" placeholder='Password' onChange={e => handleChange(e)} />
+          <input type="password" name="confirmPassword" id="pass" placeholder='Confirm password' onChange={e => handleChange(e)} />
+          <button type="submit">Create User</button>
+          <span>Already have an account ?<Link to='/login'>Login</Link></span>
         </form>
-    </FromContainer>
+      </FromContainer>
+      <ToastContainer />
     </>
   )
 }
---
+++ server/controllers/userController.js
@@ -0,0 +1,4 @@
+module.exports.register = (req, res, next) => {
+const { username, email, password } = req.body;
+const usernameCheck = await 
+}
