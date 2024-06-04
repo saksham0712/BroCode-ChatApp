bf9f43e738e768865bfe07a0fa8ffb2a2943107f
Saksham Verma <sakshamverma626@gmail.com>
2024-06-05 01:44:01 +0530

    Chat & contact Functions

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,26 +1,57 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
+import Contacts from '../components/Contacts';
+import { allUsersRoute } from '../utils/APIRoutes';
+import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
+// gjhgjhghj
 function Chat() {
+  const navigate = useNavigate();
   const [contacts, setContacts] = useState([])
   const [currentUser, setCurrentUser] = useState(undefined)
+  const [currentChat, setCurrentChat] = useState(undefined)
+  const [isLoaded, setIsLoaded] = useState(false)
   useEffect(() => {
-    const CheckVal = async()=> {
+    const CheckVal = async () => {
 
-        if (!localStorage.getItem('chat-app-user')) {
-            navigate('/');
-        }
+      if (!localStorage.getItem('chat-app-user')) {
+        navigate('/login');
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
+        setIsLoaded(true)
+      }
     }
-   CheckVal();
-}, []);
+    CheckVal();
+  }, []);
+  useEffect(() => {
+    const redirc = async () => {
+
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`)
+          setContacts(data.data)
+        } else {
+          navigate('/setAvatar')
+        }
+      }
+    }
+    redirc();
+  }, [currentUser])
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  }
   return (
     <Container>
       <div className="container">
-
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+        {
+          isLoaded && currentChat === undefined ? (<Welcome currentUser = {currentUser.username} />) : (<ChatContainer currentChat = {currentChat} />)
+        }
       </div>
     </Container>
-  ) 
+  )
 
 }
 const Container = styled.div`
@@ -43,8 +74,7 @@ background-color: #131324;
 
   }
   @media screen and (min-width: 360px) and (max-width: 480px){
-    grid-template-columns: 35% 65%;
-
+    grid-template-rows: 35% 65%; 
   }
 }
 `;
