ba66cbe363effb475ba44642e57d6890245afd0d
saksham0712 <sakshamverma626@gmail.com>
2024-06-06 15:28:48 +0530

    updated Api Fixed the bug

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -4,3 +4,4 @@ export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoutes = `${host}/api/auth/setAvatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const  sendMessageRoute = `${host}/api/messages/addmsg`;
+// hello
