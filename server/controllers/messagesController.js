17bda69259525c5e7b7540ed7f0a505b98e90b74
saksham0712 <sakshamverma626@gmail.com>
2024-06-06 18:28:25 +0530

    resolving Chat Problem

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,21 +1,38 @@
 const messageModel = require("../models/messageModel");
 
- 
 module.exports.addMessage = async (req, res, next) => {
-    try {
-        const {from,to,message} = req.body;
-        const data = await messageModel.create({
-            message: {text: message},
-            users: [from, to],
-            sender: from, 
-        })
-        console.log({from, to, message})
-        if(data) return res.json({msg: "message added successfully"})
-        return res.json({msg: "failed to  add"})
-    } catch (ex) {
-        next(ex)
-    }
-}
+  try {
+    const { from, to, message } = req.body;
+    const data = await messageModel.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+    console.log({ from, to, message });
+    if (data) return res.json({ msg: "message added successfully" });
+    return res.json({ msg: "failed to  add" });
+  } catch (ex) {
+    next(ex);
+  }
+};
 module.exports.getAllMessage = async (req, res, next) => {
-
-}
+  try {
+    const { from, to } = req.body;
+    const messages = await messageModel
+      .find({
+        users: {
+          $all: [from, to],
+        },
+      })
+      .sort({ updatedAt: 1 });
+    const projectMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
