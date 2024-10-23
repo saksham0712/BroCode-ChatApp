9ce2905430bbdd3391fa6bec8a235af0c2708e53
saksham0712 <sakshamverma626@gmail.com>
2024-10-23 22:21:46 +0530

    updated index.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+
+export const host = process.env.REACT_APP_API_URL;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoutes = `${host}/api/auth/setAvatar`;
